64ee077e34fcadc86f408ad39abfaec5f70565fc
yuya-tech <>
2020-09-08 23:29:43 +0900

    ユーザ登録画面

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,12 @@
+@section('footer')
+<footer>
+    <div class="container">
+        <div class="text-center">
+
+        <p>
+            Copyright©Techpit
+        </p>
+    </div>
+</div>
+</footer>
+@endsection
