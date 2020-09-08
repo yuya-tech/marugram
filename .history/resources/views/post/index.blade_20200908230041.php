eda706a49e4bfd23162206106c55d177f4b2a986
yuya-tech <>
2020-09-08 23:10:34 +0900

    bootstrapを参考に作成。

--- .history/resources/views/post/index.blade_20200908230041.php
+++ .history/resources/views/post/index.blade_20200908230041.php
@@ -6,13 +6,6 @@
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-
-<a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-    サインアウト<br/>
-</a>
-<form id="logout-form" action="{{route('logout')}}"method="POST"style="display:none;">
-    {{csrf_field()}}
-</form>
 <p>このページは仮のトップページです。</p>
 
 <a href="#" class="btn btn-primary">仮のボタンです</a>
