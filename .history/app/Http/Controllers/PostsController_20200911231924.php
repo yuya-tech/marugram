92af1164470aa8b7604982aa94aade5e11c07173
yuya-tech <>
2020-09-11 23:32:15 +0900

    投稿機能の実装

--- .history/app/Http/Controllers/PostsController_20200911231924.php
+++ .history/app/Http/Controllers/PostsController_20200911231924.php
@@ -10,14 +10,6 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    // ==========ここから追加する==========
-    //コンストラクタ （このクラスが呼ばれると最初にこの処理をする）
-    public function __construct()
-    {
-        // ログインしていなかったらログインページに遷移する（この処理を消すとログインしなくてもページを表示する）
-        $this->middleware('auth');
-    }
-    // ==========ここまで追加する==========
     public function index()
     {
         // テンプレート「post/index.blade.php」を表示します。
